6035444ebed4fa06df5920d90075bef008a16850
Gui Regazzo <guiregazzo@educacao.pr.gov.br>
2025-09-26 17:08:38 -0300

    feat(download): Implementada função que realiza o download do vídeo

--- src/downloader.py
+++ src/downloader.py
@@ -1,25 +1,38 @@
-from pytube import YouTube
+import yt_dlp
 import os
 import config
 import logging
 
 logging.basicConfig(level=logging.INFO)
-
 logger = logging.getLogger(__name__)
 
 def download_video(url: str) -> str:
-    yt = YouTube(url)
-    stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
-    
+    """
+    Downloads a video from the given URL using yt-dlp.
+
+    Args:
+        url (str): The URL of the video to download.
+
+    Returns:
+        str: The filename of the downloaded video.
+    """
     if not os.path.exists(config.DOWNLOAD_PATH):
         os.makedirs(config.DOWNLOAD_PATH)
-    
-    output_path = stream.download(output_path=config.DOWNLOAD_PATH)
-    logger.info(f"Video downloaded to: {output_path}")
-    return output_path
 
+    ydl_opts = {
+        'format': 'best',
+        'outtmpl': os.path.join(config.DOWNLOAD_PATH, '%(title)s.%(ext)s'),
+        'noplaylist': True,
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        filename = ydl.prepare_filename(info)
+        logger.info(f"Iniciando o download do vídeo: {info['title']}")
+        ydl.download([url])
+        logger.info(f"Download concluído: {info['title']}")
+        return filename
 
 if __name__ == "__main__":
-    test_url = "https://www.youtube.com/watch?v=hf6wIcz-gB8"
-    downloaded_file = download_video(test_url)
-    logger.info(f"Video downloaded to: {downloaded_file}")
+    test_url = "https://www.youtube.com/watch?v=R7VpCZZiVOg"
+    download_video(test_url)
